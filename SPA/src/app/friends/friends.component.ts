aa5398a9d19fb7875d9a75f9fb2277a3090a43e9
VL <exdrums@gmail.com>
2018-09-10 21:05:19 +0200

    showFriends() impl.

--- SPA/src/app/friends/friends.component.ts
+++ SPA/src/app/friends/friends.component.ts
@@ -11,7 +11,9 @@ export class FriendsComponent implements OnInit {
 
   friends: User[]; // my requests
   friendsToAccept: User[]; // requests to me
-  acceptedFriends: User[]; // my accepted requests
+
+  showedFriends: User[]; // my accepted requests
+  showOptions = 'Friends'; // Pending - Outgoing - Friends
 
 constructor(
   private route: ActivatedRoute
@@ -21,8 +23,46 @@ constructor(
     this.route.data.subscribe(data => {
       this.friends = data['friends'];
       this.friendsToAccept = data['friendsToAccept'];
-
-      // if in both then accepted
+      this.showFriends();
     });
   }
+
+  showFriends() {
+    if (this.showOptions === 'Pending') {
+      this.showedFriends = this.friendsToAccept.filter(x => {
+        let result = true;
+        this.friends.forEach(f => {
+          if (f.id !== x.id) {
+            result = false;
+          }
+        });
+        return result;
+      });
+    } else if (this.showOptions === 'Outgoing') {
+      this.showedFriends = this.friends.filter(x => {
+        let result = true;
+        this.friendsToAccept.forEach(f => {
+          if (f.id !== x.id) {
+            result = false;
+          }
+        });
+        return result;
+      });
+    } else {
+      this.showedFriends = this.friends.filter(x => {
+        let result = false;
+        this.friendsToAccept.forEach(f => {
+          if (f.id === x.id) {
+            result = true;
+          }
+        });
+        return result;
+      });
+    }
+    console.log(this.friends);
+    console.log(this.friendsToAccept);
+    console.log(this.showedFriends);
+
+  }
+
 }
